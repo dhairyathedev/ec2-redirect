6f79fc89c9a76ae9778efb035667d98282947ad8
Dhairya Shah <dtshah24022006@gmail.com>
2023-08-28 21:31:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express'
 
 const app = express()
 
-app.listen(2402, () => console.log("API Running on PORT 2402"))
+app.listen(80, () => console.log("API Running on PORT 80"))
 
 app.get('/', (req, res) => {
     res.redirect("https://dhairyashah.dev/")
